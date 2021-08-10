fe188ab83698ba7cf62e8f0fb9e530837e9c6e14
Frédéric Mohier <87375506+b2p-fred@users.noreply.github.com>
2021-08-10 15:12:55 +0200

    Tests (#7)
    * Update the application tests (still)

--- tests/App/AdminUserPagesTest.php
+++ tests/App/AdminUserPagesTest.php
@@ -27,12 +27,22 @@ class AdminUserPagesTest extends WebTestCase
 
     public function testUsersPage(): void
     {
-        // Request the building page
+        // Request the users page
         $crawler = $this->client->request('GET', '/admin/users');
 
         // Validate a successful response and some content
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextSame('h1', 'Liste des utilisateurs');
+    }
+
+    public function testUserPage(): void
+    {
+        // Request the user page
+        $crawler = $this->client->request('GET', '/admin/user/1');
+
+        // Validate a successful response and some content
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextSame('h2', 'Fiche utilisateur');
     }
 
     public function testBuildingsPage(): void
@@ -45,6 +55,26 @@ class AdminUserPagesTest extends WebTestCase
         $this->assertSelectorTextSame('h1', 'Hello buildings!');
     }
 
+    public function testBuildingPage(): void
+    {
+        // Request the building page
+        $crawler = $this->client->request('GET', '/admin/building/1');
+
+        // Validate a successful response and some content
+        $this->assertResponseIsSuccessful();
+
+        $this->assertCount(1, $crawler->filter('form[name="buildingForm"]'));
+        $this->assertCount(1, $crawler->filter('input[name="_name"]'));
+        $this->assertCount(1, $crawler->filter('input[name="_address"]'));
+        $this->assertCount(1, $crawler->filter('input[name="_zipcode"]'));
+        $this->assertCount(1, $crawler->filter('input[name="_city"]'));
+
+        $this->assertFormValue('form[name="buildingForm"]', '_name', 'Building 1');
+        $this->assertFormValue('form[name="buildingForm"]', '_address', 'Rue des fleurs');
+        $this->assertFormValue('form[name="buildingForm"]', '_zipcode', '26500');
+        $this->assertFormValue('form[name="buildingForm"]', '_city', 'Bourg-lès-Valence');
+    }
+
     public function testCompaniesPage(): void
     {
         // Request the building page
@@ -53,5 +83,20 @@ class AdminUserPagesTest extends WebTestCase
         // Validate a successful response and some content
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextSame('h1', 'Hello companies!');
+    }
+
+    public function testCompanyPage(): void
+    {
+        // Request the building page
+        $crawler = $this->client->request('GET', '/admin/company/1');
+
+        // Validate a successful response and some content
+        $this->assertResponseIsSuccessful();
+
+        $this->assertCount(1, $crawler->filter('form[name="companyForm"]'));
+        $this->assertCount(1, $crawler->filter('input[name="_name"]'));
+        $this->assertCount(1, $crawler->filter('a[href="/admin/building/1"]'));
+
+        $this->assertFormValue('form[name="companyForm"]', '_name', 'Acme 1');
     }
 }
